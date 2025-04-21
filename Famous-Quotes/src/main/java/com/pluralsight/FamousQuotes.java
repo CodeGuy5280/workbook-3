a6f12e604e7a553e7562fa68ffa84bfbf2654e8a
Alexander Leal <computerlanguage5280@gmail.com>
2025-04-21 14:49:40 -0500

    Mostly completed FamousQuotes. Need to change logic to align with Quote1 being user input 1 instead of 0.

--- Famous-Quotes/src/main/java/com/pluralsight/FamousQuotes.java
+++ Famous-Quotes/src/main/java/com/pluralsight/FamousQuotes.java
@@ -1,8 +1,24 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class FamousQuotes {
 
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String[] Quotes = {"Quote1", "Quote2", "Quote3", "Quote4", "Quote5", "Quote6", "Quote7", "Quote8", "Quote9", "Quote10"};
+
+        System.out.println("Choose a quote (1 - 10: ");
+        int i = scanner.nextInt();
+
+        if (i >= 0 && i <= Quotes.length){
+            System.out.println("Selected quote: " + Quotes[i]);
+        }else {
+            System.out.println("Invalid number.");
+        }
+
+
+        scanner.close();
 
 
 
