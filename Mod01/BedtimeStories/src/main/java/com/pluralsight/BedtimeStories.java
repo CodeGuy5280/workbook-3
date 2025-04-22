a3d62c58c478eaf24f58a4324c122594deb87c9b
Alexander Leal <computerlanguage5280@gmail.com>
2025-04-22 15:43:55 -0500

    complete payrollcalculator

--- Mod01/BedtimeStories/src/main/java/com/pluralsight/BedtimeStories.java
+++ Mod01/BedtimeStories/src/main/java/com/pluralsight/BedtimeStories.java
@@ -1,20 +1,20 @@
 package com.pluralsight;
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class BedtimeStories {
     public static void main(String[] args) {
-/*use this path
-C:\Users\AlexJ\pluralsight\workbook-3\Mod01\BedtimeStories\src\main\java\com\pluralsight\DataFiles\DataFiles*/
-        try {
 
+        try {
+            //need to assign stories to an array to access each
+//"C:/Users/AlexJ/pluralsight//workbook-3/Mod01/BedtimeStories/src/main/java/com/pluralsight/DataFiles/goldilocks.txt"
             FileInputStream fis = new FileInputStream("C:/Users/AlexJ/pluralsight//workbook-3/Mod01/BedtimeStories/src/main/java/com/pluralsight/DataFiles/goldilocks.txt");
             Scanner scanner = new Scanner(fis);
 
             while (scanner.hasNextLine()) {
                 String input = scanner.nextLine();
                 System.out.println(input);
+
             }
 
             scanner.close();
---
+++ Mod01/PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+import java.io.*;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        FileReader reader;
+        try {
+            reader = new FileReader("C:/Users/AlexJ/pluralsight/workbook-3/Mod01/BedtimeStories/src/main/java/com/pluralsight/DataFiles/employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            Employee[] employees = new Employee[8];
+            int lineIndex = 0;
+            String input;
+
+            while ((input = bufferedReader.readLine()) != null) {
+                String[] lineData = input.split("\\|");
+
+                if (lineData[0].equals("id")) {
+                    continue; // Skip header row
+                }
+
+                int id = Integer.parseInt(lineData[0]);
+                String name = lineData[1];
+                double hours = Double.parseDouble(lineData[2]);
+                double rate = Double.parseDouble(lineData[3]);
+
+                Employee newEmployee = new Employee(id, name, hours, rate);
+                employees[lineIndex] = newEmployee;
+
+                System.out.printf("ID: %d | Name: %s | Pay: $%.2f%n", newEmployee.getEmployeeId(), newEmployee.getName(), newEmployee.calculateGrossPay());
+
+                lineIndex++;
+            }
+
+            bufferedReader.close();
+            reader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
