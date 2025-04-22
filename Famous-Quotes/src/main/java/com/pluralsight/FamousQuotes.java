9e8d8ca9d9fb6cbd05cfcdac1db437955005b0ab
Alexander Leal <computerlanguage5280@gmail.com>
2025-04-22 08:53:32 -0500

    added another line to print

--- Famous-Quotes/src/main/java/com/pluralsight/FamousQuotes.java
+++ Famous-Quotes/src/main/java/com/pluralsight/FamousQuotes.java
@@ -1,27 +1,25 @@
 package com.pluralsight;
+
 import java.util.Scanner;
 
 public class FamousQuotes {
 
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-    String[] Quotes = {"That which does not kill us makes us stronger", "Without music, life would be a mistake", "God is dead. God remains dead. And we have killed him", "To live is to suffer, to survive is to find some meaning in the suffering", "We love life, not because we are used to living, but because we are used to loving", "He who has a why to live can bear almost any how", "Be careful when you fight the monsters, lest you become one", "When you gaze long into an abyss the abyss also gazes into you", "One must still have chaos in oneself to be able to give birth to a dancing star", "I have often laughed at the weaklings who thought themselves good because they had no claws"};
+        String[] Quotes = {"That which does not kill us makes us stronger", "Without music, life would be a mistake", "God is dead. God remains dead. And we have killed him", "To live is to suffer, to survive is to find some meaning in the suffering", "We love life, not because we are used to living, but because we are used to loving", "He who has a why to live can bear almost any how", "Be careful when you fight the monsters, lest you become one", "When you gaze long into an abyss the abyss also gazes into you", "One must still have chaos in oneself to be able to give birth to a dancing star", "I have often laughed at the weaklings who thought themselves good because they had no claws"};
 
-        System.out.println("Choose a quote (1 - 10: ");
+        System.out.println("Type a number to reveal a quote (1 - 10: ");
 
         int i = scanner.nextInt();
 
-        if (i >= 1 && i <= Quotes.length){
+        if (i >= 1 && i <= Quotes.length) {
             //Need to have the "- 1" to subtrack position in the array to allow user input of 1 and 10 instead of 0 - 9.
             System.out.println("Selected quote: " + Quotes[i - 1]);
-        }else {
+        } else {
             System.out.println("Invalid number.");
         }
-
-
         scanner.close();
-
 
 
     }
