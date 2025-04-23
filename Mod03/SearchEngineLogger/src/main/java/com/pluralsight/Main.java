592983ddf868248946f9b28c6d1f5c820aae0ab7
Alexander Leal <computerlanguage5280@gmail.com>
2025-04-23 14:27:15 -0500

    mostly complete

--- Mod03/SearchEngineLogger/src/main/java/com/pluralsight/Main.java
+++ Mod03/SearchEngineLogger/src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,11 @@
 package com.pluralsight;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        userLog log = new userLog();
+        log.logActions();
     }
 }
--- Mod03/SearchEngineLogger/src/main/java/com/pluralsight/userLog.java
+++ Mod03/SearchEngineLogger/src/main/java/com/pluralsight/userLog.java
@@ -1,8 +1,8 @@
 package com.pluralsight;
 
+import java.io.BufferedInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.SQLOutput;
 import java.time.LocalDateTime;
 import java.io.BufferedWriter;
 import java.time.format.DateTimeFormatter;
@@ -13,19 +13,32 @@ public class userLog {
     String logFile = "logs.txt";
     DateTimeFormatter dateTimeLog = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    Boolean userInput = true;
+    //Boolean userInput = true;
     Scanner scanner = new Scanner(System.in);
-    public void logActions(String action) {
+    public void logActions() {
 
+        //launching app
         System.out.println("Would you like to launch the application (Y/N)? ");
         String launchApp = scanner.nextLine().toUpperCase();
+
 
         if (launchApp.equals("Y")){
             String timeLog = LocalDateTime.now().format(dateTimeLog);
             String logMessage = (timeLog + "The application is launching!");
+
+            System.out.println(logMessage);
+            writeLog(logMessage);
         }else {
             System.out.println("Invalid input.");
         }
+        //search
+        System.out.println("What would you like to search?: ");
+        String searchApp = scanner.nextLine();
+        System.out.println("Searching for: " + searchApp);
+        System.out.println("Results for " + searchApp + ".");
+
+        System.out.println("Exiting the application.");
+
     }
     public void writeLog(String write){
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true))){
