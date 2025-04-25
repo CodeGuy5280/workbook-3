c57c104de06b161bd1519f3cf695b4c4aea3f59c
Alexander Leal <computerlanguage5280@gmail.com>
2025-04-25 10:33:04 -0500

    initial commit w/ relative filepath

---
+++ Mod03/Online-Store/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Relative path for file
+        String filePath = "src/main/resources/inventory.csv";
+
+
+    }
+}
